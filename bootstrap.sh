18eddae4846de9d73ef707b99736b0aa1d573c4e
itooww <itouibuki11@gmail.com>
2017-03-17 23:00:47 +0900

    print message modfy

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,13 @@
 #
 # bootstrap installs things.
 
+echo 'start distributing dotfiles ...'
+echo ''
+
 ln -s .vimrc ~/.vimrc
 ln -s .bashrc ~/.bashrc
 ln -s .tmux.conf ~/.tmux.conf
 
 echo ''
-echo '  All installed!'
+echo 'All installed!'
+
